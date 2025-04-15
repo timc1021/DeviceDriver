97ad61476e469ab69c0435393ce468130ca42382
timc1021 <iljfyy@gmail.com>
2025-04-15 10:28:33 +0900

    add exception

--- cra_250415_device_driver/device_driver.cpp
+++ cra_250415_device_driver/device_driver.cpp
@@ -7,16 +7,21 @@ DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 int DeviceDriver::read(long address)
 {
     // TODO: implement this method properly
-    int a = 0;
-    a = (int)(m_hardware->read(address));
-    a = (int)(m_hardware->read(address));
-    a = (int)(m_hardware->read(address));
-    a = (int)(m_hardware->read(address));
-    return (int)(m_hardware->read(address));
+    int a = (int)(m_hardware->read(address));
+
+    for (int i = 0; i < 4; i++) {
+        if (a != (int)(m_hardware->read(address)))
+            throw ReadFailException();
+    }
+
+    return a;
 }
 
 void DeviceDriver::write(long address, int data)
 {
     // TODO: implement this method
-    m_hardware->write(address, (unsigned char)data);
+    if (read(address) == 0xff)
+        m_hardware->write(address, (unsigned char)data);
+    else
+        throw WriteFailException();
 }
--- cra_250415_device_driver/main.cpp
+++ cra_250415_device_driver/main.cpp
@@ -5,17 +5,30 @@ using namespace testing;
 
 class FlashMock : public FlashMemoryDevice {
 public:
-	MOCK_METHOD(unsigned char, read, (long address), ());
-	MOCK_METHOD(void, write, (long address, unsigned char data), ());
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
 };
 
 TEST(DeviceDriver, ReadFromHW) {
 	FlashMock mock;
 	EXPECT_CALL(mock, read(0xbb))
-		.Times(5);
+		.Times(5)
+		.WillRepeatedly(Return(0));
 
 	DeviceDriver driver{ &mock };
 	int data = driver.read(0xbb);
+}
+
+TEST(DeviceDriver, WriteHW) {
+	FlashMock mock;
+	EXPECT_CALL(mock, read(0xbb))
+		.Times(5)
+		.WillRepeatedly(Return(0xff));
+	EXPECT_CALL(mock, write(0xbb, 0x1))
+		.Times(1);
+
+	DeviceDriver driver{ &mock };
+	driver.write(0xbb, 0x1);
 }
 
 int main() {
